a47d3be5bbd293fc277ede86496013849da0091f
Thorinka <mmlentyshenko@gmail.com>
2023-04-26 14:35:36 +0300

    Class attributes amended, classmethod get_service added, method to_json added

--- src/channel.py
+++ src/channel.py
@@ -9,8 +9,19 @@ class Channel:
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        """Экземпляр инициализирует id канала. Дальше все данные будут подтягиваться по API."""
+        self.__channel_id = channel_id
+        self.channel = Channel.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+
+        self.title = self.channel["items"][0]["snippet"]["title"]
+        self.channel_description = self.channel["items"][0]["snippet"]["description"]
+        self.url = f"https://www.youtube.com/channel/{self.__channel_id}"
+        self.subscribe_count = self.channel["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
+        self.views_count = self.channel["items"][0]["statistics"]["viewCount"]
+
+        # channel_name: str, channel_description: str, channel_url: str,
+        #                  subscribe_count: int, video_count: int, views_count: int
 
     def print_json(self, dict_to_print: dict) -> None:
         """Выводит словарь в json-подобном удобном формате с отступами"""
@@ -18,5 +29,21 @@ class Channel:
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         return self.print_json(channel)
+
+    @classmethod
+    def get_service(cls):
+        """
+        Возвращает объект для работы с YouTube API
+        """
+        return cls.youtube
+
+
+    def to_json(self, filename):
+        with open(filename, "w", encoding="utf-8") as json_file:
+            json_file.write(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+if __name__ == '__main__':
+    vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
+    print(vdud.to_json("json_file"))
