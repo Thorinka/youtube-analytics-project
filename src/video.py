f7afe7e5330b1409561fe464e378755bca19882e
Thorinka <mmlentyshenko@gmail.com>
2023-05-22 16:25:51 +0300

    try\except for Video added

--- src/video.py
+++ src/video.py
@@ -1,6 +1,7 @@
 import json
 import os
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 class Video:
     """Класс для видео"""
@@ -9,14 +10,25 @@ class Video:
 
     def __init__(self, video_id):
         """Экземпляр инициализирует id видео. Дальше все данные будут подтягиваться по API."""
-        self.__video_id = video_id
-        self.video = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                       id=video_id).execute()
+        try:
+            self.__video_id = video_id
+            self.video = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                           id=video_id).execute()
 
-        self.video_name = self.video["items"][0]["snippet"]["title"]
-        self.video_url = f"https://www.youtube.com/channel/{self.__video_id}"
-        self.view_count = self.video["items"][0]["statistics"]["viewCount"]
-        self.likes_count = self.video["items"][0]["statistics"]["likeCount"]
+            self.video_name = self.video["items"][0]["snippet"]["title"]
+            self.video_url = f"https://www.youtube.com/channel/{self.__video_id}"
+            self.view_count = self.video["items"][0]["statistics"]["viewCount"]
+            self.likes_count = self.video["items"][0]["statistics"]["likeCount"]
+        except IndexError as err:
+            self.__video_id = video_id
+            self.video = None
+
+            self.video_name = None
+            self.video_url = None
+            self.view_count = None
+            self.likes_count = None
+
+
 
     def __str__(self):
         """
